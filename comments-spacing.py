ff9979020d60ea8d767de1e605284d1cb90c50ae
coconut-irl <coconutx206@gmail.com>
2019-03-23 09:33:05 +0530

    Initial commit

---
+++ comments-spacing.py
@@ -0,0 +1,10 @@
+# Not quite PI, but a credible simulation
+print(22/7)
+
+
+# Output PI on the screen
+print(22/7) # Well, just a good approximation
+
+
+print("Something's rotten in the state of Denmark.")
+print("                              -- Shakespeare")
